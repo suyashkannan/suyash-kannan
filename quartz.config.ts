9d50006046c14cc3ea1d46b8bae24a98d2394ec7
Suyash Kannan <suyash24@iiserb.ac.in>
2025-02-26 12:57:18 +0530

    Quartz sync: Feb 26, 2025, 12:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🦖 Suyash Kannan",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
